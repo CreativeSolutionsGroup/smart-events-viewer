158127ced3354c01686ff24ed3d8b8be1e471142
masnoble <jnthomas522@gmail.com>
2023-01-30 10:15:14 -0500

    Use SWR to get the checkin numbers- no CSS

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,10 +11,24 @@ declare global {
 }
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env["GOOGLE_CLIENT_ID"],
-            clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
-        }),
-    ]
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the id_token to the token
+      if (account) {
+        token.id_token = account.id_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send send the id_token to the client
+      session.id_token = token.id_token;
+      return session;
+    },
+  },
 })
